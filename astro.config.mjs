aef59fbcf57c45cb116c30398c042c8cea528594
Lucas Nouguier <lucas.nouguier@protonmail.com>
2025-02-25 21:45:09 +0100

    added canne de combat

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-// @ts-check
-import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
